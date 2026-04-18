dc69e04a9c9ab2674d4bcfdcab4ac4c352160673
Maftuna Validjanova <maftuna.valijonova2@icloud.com>
2026-04-18 14:01:25 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/register_screen.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Workspace Booking App',
+      home: RegisterScreen(),
+    );
+  }
+}
